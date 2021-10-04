6d445e12e82d2aedf6dcae3bf42cf4894d9ca674
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-10-05 00:05:03 +0700

    Finish

---
+++ main.js
@@ -0,0 +1,113 @@
+class Suit {
+  constructor(choices) {
+    this.ROCK = "ROCK";
+    this.SCISSORS = "SCISSORS";
+    this.PAPER = "PAPER";
+    this.WIN = "WIN";
+    this.LOSE = "LOSE";
+    this.DRAW = "DRAW";
+    this.choices = choices;
+  }
+
+  setDisabledClass() {
+    this.choices.forEach(ch => ch.parentElement.classList.add("disabled"));
+  }
+
+  setRemoveDisabledClass() {
+    this.choices.forEach(ch => ch.parentElement.classList.remove("disabled"));
+  }
+
+  get computerChoice() {
+    const comp = Math.random();
+    if (comp < 0.34) return this.ROCK;
+    if (comp >= 0.34 && comp < 0.67) return this.SCISSORS;
+    return this.PAPER;
+  }
+
+  setRandomImages(imgComp) {
+    const images = [this.ROCK, this.SCISSORS, this.PAPER];
+
+    let i = 0;
+    const dateStart = new Date().getTime();
+    setInterval(function () {
+      if (new Date().getTime() - dateStart > 1000) {
+        clearInterval;
+        return;
+      }
+      imgComp.setAttribute("src", "img/" + images[i++] + ".png");
+      if (i == images.length) i = 0;
+    }, 100);
+  }
+
+  getResult(computer, player) {
+    if (player == computer) return this.DRAW;
+    if (player == this.ROCK)
+      return computer == this.SCISSORS ? this.WIN : this.LOSE;
+    if (player == this.SCISSORS)
+      return computer == this.ROCK ? this.LOSE : this.WIN;
+    if (player == this.SCISSORS)
+      return computer == this.PAPER ? this.WIN : this.LOSE;
+    if (player == this.PAPER)
+      return computer == this.SCISSORS ? this.LOSE : this.WIN;
+  }
+}
+
+const computerScore = document.querySelector(".score-value-comp");
+const playerScore = document.querySelector(".score-value-user");
+const choices = document.querySelectorAll(".player-options .player-option img");
+const playerImg = document.querySelector(".result-user img");
+const computerImg = document.querySelector(".result-computer img");
+const resultStart = document.querySelector(".result-start");
+const resultChoices = document.querySelector(".result-end");
+const resultMessage = document.querySelector(".result-textbox h1");
+
+const suitClass = new Suit(choices);
+
+[...choices].map(choice => {
+  choice.addEventListener("click", e => {
+    suitClass.setDisabledClass();
+    resultStart.classList.add("hide");
+    resultChoices.classList.remove("hide");
+
+    // const player
+    const compChoice = suitClass.computerChoice;
+    const playerChoice = e.target.id;
+    const result = suitClass.getResult(compChoice, playerChoice);
+
+    playerImg.setAttribute("src", "img/" + playerChoice + ".png");
+    suitClass.setRandomImages(computerImg);
+
+    setTimeout(() => {
+      let info = "";
+
+      computerImg.setAttribute("src", "img/" + compChoice + ".png");
+
+      switch (result) {
+        case "WIN":
+          playerScore.textContent = +playerScore.textContent + 1;
+          resultMessage.className = "win";
+          info = `You Win`;
+          break;
+        case "LOSE":
+          computerScore.textContent = +computerScore.textContent + 1;
+          resultMessage.className = "lose";
+          info = `You Lose`;
+          break;
+        case "DRAW":
+          resultMessage.className = "draw";
+          info = `Draw`;
+          break;
+        default:
+          info = `Something went wrong`;
+          break;
+      }
+
+      resultMessage.parentElement.classList.remove("hide");
+      resultMessage.innerHTML = info;
+
+      suitClass.setRemoveDisabledClass();
+
+      console.log(result);
+    }, 1000);
+  });
+});
--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 function getPilahanComputer() {
-    const comp = Math.random();
-    if (comp < 0.34) return 'gajah';
-    if (comp >= 0.34 && comp < 0.67) return 'orang';
-    return 'semut';
+  const comp = Math.random();
+  if (comp < 0.34) return "gajah";
+  if (comp >= 0.34 && comp < 0.67) return "orang";
+  return "semut";
 }
 
 function getHasil(comp, Player) {
-    if (Player == comp) return 'SERI!';
-    if (Player == 'gajah') return (comp == 'orang') ? 'MENANG!' : 'KALAH!';
-    if (Player == 'orang') return (comp == 'gajah') ? 'KALAH!' : 'MENANG!';
-    if (Player == 'orang') return (comp == 'semut') ? 'MENANG!' : 'KALAH!';
-    if (Player == 'semut') return (comp == 'orang') ? 'KALAH!' : 'MENANG!';
+  if (Player == comp) return "SERI!";
+  if (Player == "gajah") return comp == "orang" ? "MENANG!" : "KALAH!";
+  if (Player == "orang") return comp == "gajah" ? "KALAH!" : "MENANG!";
+  if (Player == "orang") return comp == "semut" ? "MENANG!" : "KALAH!";
+  if (Player == "semut") return comp == "orang" ? "KALAH!" : "MENANG!";
 }
 
 function GmbAcak() {
-    const imgComp = document.querySelector('.img-komputer');
-    const gambar = ['gajah', 'semut', 'orang'];
-    let i = 0;
-    const waktuMulai = new Date().getTime();
-    setInterval(function () {
-        if (new Date().getTime() - waktuMulai > 1000) {
-            clearInterval;
-            return;
-        }
-        imgComp.setAttribute('src', 'img/' + gambar[i++] + '.png');
-        if (i == gambar.length) i = 0;
-    }, 100)
+  const imgComp = document.querySelector(".img-komputer");
+  const gambar = ["gajah", "semut", "orang"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
+    }
+    imgComp.setAttribute("src", "img/" + gambar[i++] + ".png");
+    if (i == gambar.length) i = 0;
+  }, 100);
 }
 
-const pilihan = document.querySelectorAll('li img');
+const pilihan = document.querySelectorAll("li img");
 pilihan.forEach(function (pil) {
-    pil.addEventListener('click', function () {
-        const pilihanComputer = getPilahanComputer();
-        const pilihaPlayer = pil.className;
-        const hasil = getHasil(pilihanComputer, pilihaPlayer);
+  pil.addEventListener("click", function () {
+    const pilihanComputer = getPilahanComputer();
+    const pilihaPlayer = pil.className;
+    const hasil = getHasil(pilihanComputer, pilihaPlayer);
 
-        GmbAcak();
+    GmbAcak();
 
-        setTimeout(function () {
-            const imgComp = document.querySelector('.img-komputer');
-            imgComp.setAttribute('src', 'img/' + pilihanComputer + '.png');
+    setTimeout(function () {
+      const imgComp = document.querySelector(".img-komputer");
+      imgComp.setAttribute("src", "img/" + pilihanComputer + ".png");
 
-            const info = document.querySelector('.info');
-            info.innerHTML = hasil;
-        }, 1000);
-        return hasil;
-
-
-    });
+      const info = document.querySelector(".info");
+      info.innerHTML = hasil;
+    }, 1000);
+    return hasil;
+  });
 });
 
 // const pGajah = document.querySelector('.gajah');
@@ -88,3 +86,20 @@ pilihan.forEach(function (pil) {
 //     const info = document.querySelector('.info');
 //     info.innerHTML = hasil;
 // });
+
+const randomImg = () => {
+  const imgComp = document.querySelector(".result-computer-img");
+  const gambar = ["ROCK", "PAPER", "SCISSORS"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    // if (new Date().getTime() - waktuMulai > 1000) {
+    //   clearInterval;
+    //   return;
+    // }
+    imgComp.setAttribute("src", "img/" + gambar[i++] + ".png");
+    if (i == gambar.length) i = 0;
+  }, 120);
+};
+
+randomImg();
